0d8e8e9e3619ee4177088e13ab84ca6c1f05b923
Jaidyn Belbin <jaidyn.belbin797@gmail.com>
2021-01-18 15:48:44 +0800

    Top navigation tabs implemented

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,11 +1,14 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import { ColorSchemeName, View } from 'react-native';
+import Colors from '../constants/Colors';
+import { Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 
 // If you are not familiar with React Navigation, we recommend going through the
@@ -26,9 +29,38 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
+    <Stack.Navigator screenOptions={{ 
+      headerStyle: {
+          backgroundColor: Colors.light.tint,
+          shadowOpacity: 0,
+          elevation: 0,
+      },
+      headerTintColor: Colors.light.background,
+      headerTitleAlign: 'left',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        
+      }
+      }}>
+        
+      <Stack.Screen 
+      name="Root" 
+      component={MainTabNavigator}
+      options = {{
+        title: 'WhatsApp',
+        headerRight: () => (
+          <View style={{ 
+            flexDirection: 'row', 
+            width: 60, 
+            justifyContent: 'space-between', 
+            marginRight: 10}}>
+            <Octicons name = "search" size={22} color = "white" />
+            <MaterialCommunityIcons name = "dots-vertical" size={22} color = "white" />
+          </View>
+        )
+      }} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
+
   );
 }
