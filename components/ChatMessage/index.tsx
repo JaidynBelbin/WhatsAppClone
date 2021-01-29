ee69f75b1d2811840f906ff2506bee07c1840377
Jaidyn Belbin <jaidyn.belbin797@gmail.com>
2021-01-29 12:59:36 +0800

    fetch and display messages

--- components/ChatMessage/index.tsx
+++ components/ChatMessage/index.tsx
@@ -6,44 +6,44 @@ import {Message} from '../../types';
 
 
 export type ChatMessageProps = {
-    message: Message;
+  message: Message;
+  myID: String,
 }
 
-
 const ChatMessage = (props: ChatMessageProps) => {
-
-    const {message} = props;
-
-    const isMyMessage = () => {
-        return message.user.id === 'u1';
-    }
-
-    return (
-        <View style={styles.container}>
-
-            {/** 
-             * Implementing conditional styling for the messageBox depending on the result
-             * of isMyMessage
-            */}
-
-            <View style={[
-                styles.messageBox, {
-                    backgroundColor: isMyMessage() ? '#dcf8c5' : 'white',
-                    marginLeft: isMyMessage() ? 50 : 0,
-                    marginRight: isMyMessage() ? 0 : 50,
-                }
-                ]}>
-
-                {/**
-                 * Conditionally rendering the username only if the message is
-                 * not from me. */}
-
-                {!isMyMessage() && <Text style = {styles.name}>{message.user.name}</Text>}
-                <Text style = {styles.message}>{message.content}</Text>
-                <Text style = {styles.time}>{moment(message.createdAt).fromNow()}</Text>
-            </View>
-        </View>
-    )
+  
+  const {message, myID} = props;
+  
+  const isMyMessage = () => {
+    return message.user.id === myID;
+  }
+  
+  return (
+    <View style={styles.container}>
+      
+      {/**
+       * Implementing conditional styling for the messageBox depending on the result
+       * of isMyMessage
+       */}
+      
+      <View style={[
+        styles.messageBox, {
+          backgroundColor: isMyMessage() ? '#dcf8c5' : 'white',
+          marginLeft: isMyMessage() ? 50 : 0,
+          marginRight: isMyMessage() ? 0 : 50,
+        }
+      ]}>
+        
+        {/**
+         * Conditionally rendering the username only if the message is
+         * not from me. */}
+        
+        {!isMyMessage() && <Text style = {styles.name}>{message.user.name}</Text>}
+        <Text style = {styles.message}>{message.content}</Text>
+        <Text style = {styles.time}>{moment(message.createdAt).fromNow()}</Text>
+      </View>
+    </View>
+  )
 }
 
 export default ChatMessage;
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,37 +1,78 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, ImageBackground, View} from 'react-native';
 
 import {useRoute} from '@react-navigation/native';
+
+import {
+  API,
+  Auth,
+  graphqlOperation
+} from 'aws-amplify';
+
+import {messagesByChatRoom} from "../src/graphql/queries";
 
 import chatRoomData from '../data/Chats';
 import ChatMessage from '../components/ChatMessage';
 import BG from '../assets/images/BG.png';
 import InputBox from '../components/InputBox';
 
+
 const ChatRoomScreen = () => {
-
-    const route = useRoute();
+  
+  const [messages, setMessages] = useState([]);
+  const [myID, setMyID]  = useState(null);
+  
+  const route = useRoute();
+  console.log(route.params.id);
+  
+  useEffect(() => {
     
+    const fetchMessages = async () => {
+      const messagesData = await API.graphql(
+        graphqlOperation(
+          messagesByChatRoom, {
+            chatRoomID: route.params.id,
+            sortDirection: "DESC",
+          }
+        )
+      )
+      
+      setMessages(messagesData.data.messagesByChatRoom.items);
+      
+    }
     
+    fetchMessages();
+  }, [])
+  
+  useEffect(() => {
+    
+    const getMyID = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser();
+      setMyID(userInfo.attributes.sub);
+    }
+    
+    getMyID();
+  }, [])
+  
+  
+  return (
 
-    return (
+      <ImageBackground style={{width: '100%', height: '100%'}} source={BG}>
+          <View style={{
+              flex: 1,
+          }}>
 
-        <ImageBackground style={{width: '100%', height: '100%'}} source={BG}>
-            <View style={{
-                flex: 1,
-            }}>
+              <FlatList
+                  data={messages}
+                  renderItem={({ item }) => <ChatMessage myID = {myID} message={item}/>}
+                  inverted
+              />
 
-                <FlatList
-                    data={chatRoomData.messages}
-                    renderItem={({ item }) => <ChatMessage message={item}/>}
-                    inverted
-                />
+          </View>
+          <InputBox chatRoomID = {route.params.id}/>
+      </ImageBackground>
 
-            </View>
-            <InputBox chatRoomID = {route.params.id}/>
-        </ImageBackground>
-
-    )
+  )
 }
 
 export default ChatRoomScreen;
